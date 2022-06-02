448d2c3bd991901c7a000b80bea3e92603b23dab
328 GameLab <83389425+Holywater-lee@users.noreply.github.com>
2022-06-02 16:52:24 +0900

    Galaga Test
    asdf

--- HoseoSDL/src/main.cpp
+++ HoseoSDL/src/main.cpp
@@ -1,31 +1,20 @@
-#include "Game.h" 
-#include <iostream>
+#pragma comment(lib, "SDL2.lib")
+#pragma comment(lib, "SDL2main.lib")
+#pragma comment(lib, "SDL2_image.lib")
+#pragma comment(lib, "SDL2_ttf.lib")
+#pragma comment(lib, "SDL2_mixer.lib")
 
-const int FPS = 60;
-const int DELAY_TIME = 1000.0f / FPS;
+#include "GameManager.h"
 
-int main(int argc, char* argv[])
-{
-    if (TheGame::Instance()->setup())
-    {
-        Uint32 frameStart, frameTime;
-        while (TheGame::Instance()->running()) {
-            frameStart = SDL_GetTicks();     // step 1
 
-            TheGame::Instance()->handleEvents();
-            TheGame::Instance()->update();
-            TheGame::Instance()->render();
+int main(int argc, char* argv[]) {
 
-            frameTime = SDL_GetTicks() - frameStart;  // step 2
-            if (frameTime < DELAY_TIME) {
-                SDL_Delay((int)(DELAY_TIME - frameTime)); // step 3
-            }
-        }
-    }
-    else {
-        std::cerr << "game init failure %s" << SDL_GetError();
-        return -1;
-    }
-    TheGame::Instance()->clean();
-    return 0;
+	QuickSDL::GameManager* game = QuickSDL::GameManager::Instance();
+
+	game->Run();
+
+	QuickSDL::GameManager::Release();
+	game = NULL;
+
+	return 0;
 }
